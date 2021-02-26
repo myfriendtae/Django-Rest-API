0cb580707c6ad3fd261fc0f70abc36333634efdb
Tae Park <myfriendtae@gmail.com>
2021-02-26 22:25:54 +1300

    inital commit

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import routers, serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+    
+
