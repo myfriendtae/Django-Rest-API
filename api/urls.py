fa284c7d27e467c4d2f503ac79f7e815eb13af79
Tae Park <myfriendtae@gmail.com>
2021-02-28 18:06:43 +1300

    add HelloAPIView

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,6 @@ router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('test-api/', views.HelloApiView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,25 @@ from .serializers import UserSerializer
 from rest_framework import viewsets
 from .models import UserProfile
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserSerializer
+
+
+class HelloApiView(APIView):
+    """Test API view"""
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP moethods as function (get, post, patch, put, delete)',
+            'Is similar to a triditional django view',
+            'Gives you the most control over you application logic', 
+            'is mapped manually to URLs'
+        ]
+        return Response({'message': 'hello', 'an_apiview': an_apiview})
