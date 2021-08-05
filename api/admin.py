0996649ba827c7e76248dee40ae5086885c21559
Tae Park <myfriendtae@gmail.com>
2021-08-05 21:51:11 +1200

    update admin to support customer admin model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 from api import models
 
-admin.site.register(models.UserProfile)
 admin.site.register(models.ProfileFeedItem)
 admin.site.register(models.Movie)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')},
+        ),
+        (
+            _('Important dates'),
+            {'fields': ('last_login',)}
+        )
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.UserProfile, UserAdmin)
