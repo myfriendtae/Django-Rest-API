8aa3420105dc569d9d6ac110d47a1a20b1b1424f
Taesuk Park <myfriendtae@gmail.com>
2021-08-08 18:22:46 +1200

    remove unnecessary lines

--- api/serializers.py
+++ api/serializers.py
@@ -21,11 +21,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def create(self, **validated_data):
         """Create and return a new user"""
-        # user = models.UserProfile.objects.create_user(
-        #     email=validated_data['email'],
-        #     name=validated_data['name'],
-        #     password=validated_data['password']
-        # )
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
         user = models.UserProfile.objects.create_user(**validated_data)
         return user
 
